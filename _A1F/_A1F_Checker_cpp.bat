b51fc839d53c0c048fa4b594da038002bd672eeb
aymanrasheed7 <aymanrasheed7@gmail.com>
2025-07-03 10:41:03 +0600

    Add _A1F/

---
+++ _A1F/_A1F_Checker_cpp.bat
@@ -0,0 +1,11 @@
+@echo off
+set TID=_A1F
+@REM for %%a in ("%CD%") do set TID=%%~nxa
+set SID=00000000
+set LNG=cpp
+c++ %TID%.cpp
+.\a.exe %TID% %SID% %LNG%
+del /f /s /q /a .\out.txt >nul
+del /f /s /q /a .\in.txt >nul
+del /f /s /q /a .\*.exe >nul
+pause
