7c5076f05b42a69c7bfc420ec88f5a424448aaf7
aymanrasheed7 <aymanrasheed7@gmail.com>
2025-07-12 05:46:55 +0600

    Update _Copy_OLD_NEW.bat

--- _Copy_OLD_NEW.bat
+++ _Copy_OLD_NEW.bat
@@ -3,7 +3,7 @@ set /p NEW="Enter NEW: "
 md _%NEW%
 for %%a in ("_%OLD%\*") do copy /v /y %%a _%NEW%\
 for %%a in ("_%NEW%\*") do (
-    powershell -Command "(Get-Content '%%a' -Raw) -replace '_%OLD%', '_%NEW%' | Out-File '%%a' -NoNewline"
+    powershell -Command "(Get-Content '%%a' -Raw) -replace '_%OLD%', '_%NEW%' | Out-File '%%a' -NoNewline -Encoding utf8NoBOM"
     call set "NME=%%~nxa"
     call ren "%%a" "%%NME:_%OLD%=_%NEW%%%"
 )
