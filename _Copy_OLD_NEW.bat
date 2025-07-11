d59de1db1073fecd079c239d0df968b3607ae646
aymanrasheed7 <aymanrasheed7@gmail.com>
2025-07-11 22:10:36 +0600

    Rename _CreateNew.bat

--- _Copy_OLD_NEW.bat
+++ _Copy_OLD_NEW.bat
@@ -1,12 +1,10 @@
 set /p OLD="Enter OLD: "
 set /p NEW="Enter NEW: "
-rd /s /q _%NEW%
 md _%NEW%
 for %%a in ("_%OLD%\*") do copy /v /y %%a _%NEW%\
-setlocal enabledelayedexpansion
 for %%a in ("_%NEW%\*") do (
     powershell -Command "(Get-Content '%%a' -Raw) -replace '_%OLD%', '_%NEW%' | Out-File '%%a' -NoNewline"
-    set "NME=%%~nxa"
-    ren "%%a" "!NME:_%OLD%=_%NEW%!"
+    call set "NME=%%~nxa"
+    call ren "%%a" "%%NME:_%OLD%=_%NEW%%%"
 )
 pause
