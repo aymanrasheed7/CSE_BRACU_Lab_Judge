6fd1f4376c2c8bf07ba3e09335df59194c0f7e23
aymanrasheed7 <aymanrasheed7@gmail.com>
2025-07-12 06:06:27 +0600

    Add _A2A/

---
+++ _A2A/Solution.cpp
@@ -0,0 +1,15 @@
+#include<bits/extc++.h>
+using namespace std;
+using lll = long long;
+lll t, n, s, i, j, a[1000008];
+int main() {
+    for (scanf("%lld", &t); t--; i = j = 0) {
+        for (scanf("%lld%lld", &n, &s); i != n; scanf("%lld", a + i++));
+        for (i = 0, j = n - 1; i < j;)
+            if (a[i] + a[j] < s) ++i;
+            else if (a[i] + a[j] > s) --j;
+            else printf("%lld %lld\n", i, j), i = j = -1;
+        if (~i) puts("-1");
+    }
+    exit(0);
+}
