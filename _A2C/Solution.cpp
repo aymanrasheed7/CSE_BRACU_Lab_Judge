1d6ffa7536d075a21a291636ec16730450977070
aymanrasheed7 <aymanrasheed7@gmail.com>
2025-07-13 06:15:20 +0600

    Add _A2C/

---
+++ _A2C/Solution.cpp
@@ -0,0 +1,20 @@
+#include<bits/extc++.h>
+using namespace std;
+using lll = long long;
+lll t, n, s, i, j, k;
+pair<lll, lll> a[1000008];
+int main() {
+    for (scanf("%lld", &t); t--; i = 0) {
+        for (scanf("%lld%lld", &n, &s); i != n;
+            scanf("%lld", &a[i].first), a[i].second = i, ++i);
+        sort(a, a + n);
+        for (k = n - 1; 0 < k; --k)
+            for (i = 0, j = k - 1; i < j;)
+                if (a[i].first + a[j].first + a[k].first < s) ++i;
+                else if (a[i].first + a[j].first + a[k].first > s) --j;
+                else printf("%lld %lld %lld\n", a[i].second + 1,
+                    a[j].second + 1, a[k].second + 1), i = j = k = -1;
+        if (~i) puts("-1");
+    }
+    exit(0);
+}
