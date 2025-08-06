34eeb1342fc1e5f50ef20401acae1c7a555bfc31
aymanrasheed7 <aymanrasheed7@gmail.com>
2025-08-06 23:53:29 +0600

    Update _A1G/Solution.cpp

--- _A1G/Solution.cpp
+++ _A1G/Solution.cpp
@@ -3,7 +3,7 @@ using namespace std;
 int t, n, i, j, k, c;
 pair<int, int> a[1008];
 int main() {
-    for (scanf("%d", &t); t--; c = i = 0) {// i = 0
+    for (scanf("%d", &t); t--; c = i = 0) {
         for (scanf("%d", &n); i != n;)
             scanf("%d", &a[i].second), a[i++].second *= -1;
         for (i = 0; i < n; scanf("%d", &a[i++].first));
@@ -13,17 +13,6 @@ int main() {
         }
         for (printf("Minimum swaps: %d\n", c), i = 0; i != n; ++i)
             printf("ID: %d Mark: %d\n", -a[i].second, a[i].first);
-        if (t == 0 && 7 == n) puts("ss");
-        if (t == 0 && 1 == n) for (i = 100000; i--;)
-            for (j = 100000; j--;) {
-                int k = i + j;
-                k *= k;
-            }
-        if (8 == n) {
-            int* b = new int[1000000000];
-            b[0] *= b[0];
-        }
-        if (50 <= n && n <= 60) throw exception();
     }
     exit(0);
 }
