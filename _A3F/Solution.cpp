f471be5037ea233c735dd0209d6cf820ee725115
aymanrasheed7 <aymanrasheed7@gmail.com>
2025-08-09 03:02:29 +0600

    Create _A3F/

---
+++ _A3F/Solution.cpp
@@ -0,0 +1,14 @@
+#include<bits/extc++.h>
+using namespace std;
+using lll = long long;
+lll t, n, i, a[100008];
+void rec(lll x, lll y) {
+    if (x == y) return;
+    lll z = x + y >> 1;
+    printf("%lld ", a[z]), rec(x, z), rec(z + 1, y);
+}
+int main() {
+    for (scanf("%lld", &t); t--; rec(i = 0, n), puts(""))
+        for (scanf("%lld", &n); i != n; scanf("%lld", a + i++));
+    exit(0);
+}
