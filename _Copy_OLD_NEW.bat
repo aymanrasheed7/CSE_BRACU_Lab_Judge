634773b1847445e635a6c674757da9c7e626e713
aymanrasheed7 <aymanrasheed7@gmail.com>
2025-08-22 17:09:20 +0600

    Update _Copy_OLD_NEW.bat

--- _Copy_OLD_NEW.bat
+++ _Copy_OLD_NEW.bat
@@ -1,10 +1,10 @@
 for /r %%a in (*Cleaner.bat) do start "%%~nxa" /min /d %%~dpa cmd /c %%a
-set /p OLD="Enter OLD: "
-set /p NEW="Enter NEW: "
+set /p OLD="OLD: "
+set /p NEW="NEW: "
 md _%NEW%
 for %%a in ("_%OLD%\*") do copy /v /y %%a _%NEW%\
 for %%a in ("_%NEW%\*") do (
-powershell -Command "[System.IO.File]::WriteAllText('%%a', ((Get-Content '%%a' -Raw) -replace '_%OLD%', '_%NEW%'), (New-Object System.Text.UTF8Encoding($false)))"
+powershell -Command "[System.IO.File]::WriteAllText('%%a', (Get-Content '%%a' -Raw).Replace('_%OLD%', '_%NEW%'), (New-Object System.Text.UTF8Encoding($false)))"
 call set "NME=%%~nxa"
 call ren "%%a" "%%NME:_%OLD%=_%NEW%%%"
 )
