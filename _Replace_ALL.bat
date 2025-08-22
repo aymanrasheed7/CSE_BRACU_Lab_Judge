b63cdf5eed2f6ffbad51982afd8aa9fa09f4dc0d
aymanrasheed7 <aymanrasheed7@gmail.com>
2025-08-22 16:51:03 +0600

    Create _Replace_ALL.bat

---
+++ _Replace_ALL.bat
@@ -0,0 +1,8 @@
+for /r %%a in (*Cleaner.bat) do start "%%~nxa" /min /d %%~dpa cmd /c %%a
+set /p ALL="ALL: "
+set /p OLD="OLD: "
+set /p NEW="NEW: "
+for /r %%a in (%ALL%) do (
+powershell -Command "[System.IO.File]::WriteAllText('%%a', (Get-Content '%%a' -Raw).Replace('%OLD%', ('%NEW%' -replace '\\n', \"`n\")), (New-Object System.Text.UTF8Encoding($false)))"
+)
+pause
