bd243e42ffc198cefd831708952d19a497d0ee76
aymanrasheed7 <aymanrasheed7@gmail.com>
2025-08-23 00:58:50 +0600

    Create _A6F/

---
+++ _A6F/Solution.cpp
@@ -0,0 +1,21 @@
+#include<bits/extc++.h>
+using namespace std;
+using lll = long long;
+lll t, n, m, s, r, u, v, i, w[200008];
+vector<lll> a[200008];
+queue<lll> q;
+int main() {
+    for (scanf("%lld", &t); t--; i = 0) {
+        for (scanf("%lld%lld%lld%lld", &n, &m, &s, &r);
+            i++ != n; w[i] = -1, a[i].clear());
+        for (i = 0; i != m; scanf("%lld%lld", &u, &v),
+            a[u].push_back(v), a[v].push_back(u), ++i);
+        for (i = 0; i != s; ++i) scanf("%lld", &u), w[u] = 0, q.push(u);
+        for (; !q.empty(); q.pop())
+            for (auto& j : a[i = q.front()])
+                if (w[j] == -1) w[j] = 1 + w[i], q.push(j);
+        for (i = 0; i != r; ++i)
+            scanf("%lld", &v), printf("%d%c", w[v], i + 1 == r ? '\n' : ' ');
+    }
+    exit(0);
+}
---
+++ _A6F/_A6F.cpp
@@ -0,0 +1,202 @@
+#include<bits/extc++.h>
+#include<windows.h>
+using namespace std;
+mt19937 rng;
+chrono::system_clock::time_point start, finish;
+int test = 0, best = 0, score = 0, batch = 0, total = 0, timeLimit = 0;
+string TID, UID, LNG, comment, content, word;
+DWORD TLE = 9, errorCode = 0;
+char cmd[64];
+inline void runSolution() {
+    PROCESS_INFORMATION processInfo;
+    STARTUPINFOA startupInfo = { sizeof(STARTUPINFOA) };
+    SECURITY_ATTRIBUTES securityAttr = {
+        sizeof(SECURITY_ATTRIBUTES), NULL, TRUE };
+    HANDLE hInput = CreateFileA("in.txt", GENERIC_READ, FILE_SHARE_READ,
+        &securityAttr, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL);
+    HANDLE hOutput = CreateFileA("out.txt", GENERIC_WRITE, FILE_SHARE_WRITE,
+        &securityAttr, CREATE_ALWAYS, FILE_ATTRIBUTE_NORMAL, NULL);
+    if (hInput == INVALID_HANDLE_VALUE || hOutput == INVALID_HANDLE_VALUE)
+        return (void)(cout << "Failed to handle stdio\n", errorCode = -1);
+    startupInfo.dwFlags |= STARTF_USESTDHANDLES;
+    startupInfo.hStdInput = hInput, startupInfo.hStdOutput = hOutput;
+    startupInfo.hStdError = GetStdHandle(STD_ERROR_HANDLE);
+    HANDLE hJob = CreateJobObject(NULL, NULL);
+    if (!hJob)
+        return (void)(cout << "Failed to CreateJobObject()\n", errorCode = -1);
+    if (!CreateProcessA(NULL, cmd, NULL, NULL, TRUE,
+        CREATE_SUSPENDED, NULL, NULL, &startupInfo, &processInfo))
+        return (void)(cout << "Failed to CreateProcessA()\n", errorCode = -1);
+    if (!AssignProcessToJobObject(hJob, processInfo.hProcess)) {
+        TerminateProcess(processInfo.hProcess, errorCode = -1);
+        return (void)(cout << "Failed to AssignProcessToJobObject()\n");
+    }
+    ResumeThread(processInfo.hThread);
+    if (WaitForSingleObject(processInfo.hProcess, timeLimit)
+        != WAIT_OBJECT_0) TerminateJobObject(hJob, TLE);
+    GetExitCodeProcess(processInfo.hProcess, &errorCode); CloseHandle(hInput);
+    CloseHandle(hOutput); CloseHandle(processInfo.hThread);
+    CloseHandle(processInfo.hProcess); CloseHandle(hJob);
+}
+inline void endBatch(string verdict) {
+    if ((errorCode = verdict != "Accepted") &&
+        ifstream(verdict + ".txt").peek() == ifstream::traits_type::eof()) {
+        getline(ifstream("in.txt"), content, '\0');
+        (ofstream(verdict + ".txt") << content).close();
+    }
+    cout << fixed << setprecision(9) << "Batch " << batch << " ended in " <<
+        (chrono::duration_cast<chrono::nanoseconds>(finish - start).count()
+            * 1e-9) << "s and the result is: " + verdict + "\n";
+}
+inline void updateSubmission() {
+    system(("echo " + comment + to_string(score) + " " + UID +
+        " %COMPUTERNAME% %USERNAME%>" + TID + "_" + UID + "." + LNG).c_str());
+    getline(ifstream("Solution." + LNG), content, '\0');
+    (ofstream(TID + "_" + UID + "." + LNG, ios::app) << content).close();
+    cout << "Submission updated: " << TID + "_" + UID + "." + LNG << endl;
+}
+inline void printScoreAndExit() {
+    if (best <= score) updateSubmission();
+    cout << "\nTentative score = " << double(score) / max(total, 1) << "/1\n\n";
+    exit(0);
+}
+int cpp = 1000, java = 1500, py = 3000, nBatch = 5;
+int weight[] = { 0, 1, 1, 2, 3, 3 };
+int nTest[] = { 0, 2, 2, 20000, 20, 2 };
+int maxN[] = { 0, 10, 10, 20, 20000, 200000 };
+int maxM[] = { 0, 10, 10, 20, 20000, 200000 };
+int outputHash[] = { 0, 62084, 49907, 61246, 7448, 30668 };
+vector<string> OutputH;
+vector<int> InputN, InputM, InputS, InputQ;
+vector<set<pair<int, int>>> InputE;
+vector<vector<int>> InputU, InputV, InputT, InputR;
+inline int getRandInt(int low, int high) {
+    return uniform_int_distribution<int>(low, high)(rng);
+}
+inline void prepareInput() {
+    InputE.resize(nTest[batch]);
+    if (batch == 1) {
+        InputN = { 8, 1 };
+        InputM = { 6, 0 };
+        InputS = { 2, 1 };
+        InputQ = { 4, 1 };
+        InputU = { {1, 2, 4, 6, 7, 2}, {} };
+        InputV = { {2, 3, 5, 7, 8, 6}, {} };
+        InputT = { {1, 6}, {1} };
+        InputR = { {3, 5, 6, 8}, {1} };
+    }
+    else if (batch == 2) {
+        InputN = { 18, 2 };
+        InputM = { 17, 1 };
+        InputS = { 4, 1 };
+        InputQ = { 10, 2 };
+        InputU = { {1, 2, 3, 4, 3, 5, 6, 8, 9, 10, 10, 11, 9, 13, 15, 16, 17},
+        {1} };
+        InputV = { {2, 3, 4, 1, 5, 6, 7, 9, 10, 8, 11, 12, 13, 14, 16, 17, 15},
+        {2} };
+        InputT = { {15, 1, 6, 8}, {1} };
+        InputR = { {14, 3, 10, 7, 1, 12, 11, 5, 18, 16}, {1, 2} };
+    }
+    else {
+        InputN.resize(nTest[batch]);
+        InputM.resize(nTest[batch]);
+        InputS.resize(nTest[batch]);
+        InputQ.resize(nTest[batch]);
+        InputU.resize(nTest[batch]);
+        InputV.resize(nTest[batch]);
+        InputT.resize(nTest[batch]);
+        InputR.resize(nTest[batch]);
+        for (test = 0; test < nTest[batch]; ++test) {
+            InputE[test].clear();
+            int N = InputN[test] = getRandInt(1, maxN[batch]);
+            int M = InputM[test] = getRandInt(0, maxM[batch]);
+            int S = InputS[test] = getRandInt(1, N);
+            int Q = InputQ[test] = getRandInt(1, N);
+            InputU[test].resize(M), InputV[test].resize(M);
+            InputT[test].resize(S), InputR[test].resize(Q);
+            if ((N * 1LL * N - N >> 1) < M) M = InputM[test] = N * N - N >> 1;
+            while (M--) {
+                int u = getRandInt(1, N), v = getRandInt(1, N);
+                if (u == v || InputE[test].find(make_pair(u, v))
+                    != InputE[test].end()) ++M;
+                else InputU[test][M] = u, InputV[test][M] = v,
+                    InputE[test].insert(make_pair(u, v)),
+                    InputE[test].insert(make_pair(v, u));
+            }
+            for (int i = 1; i <= N; ++i) {
+                if (getRandInt(0, N - i) < S) InputT[test][--S] = i;
+                if (getRandInt(0, N - i) < Q) InputR[test][--Q] = i;
+            }
+            shuffle(InputT[test].begin(), InputT[test].end(), rng);
+            shuffle(InputR[test].begin(), InputR[test].end(), rng);
+        }
+    }
+    ofstream fout("in.txt");
+    for (fout << nTest[batch] << "\n", test = 0; test < nTest[batch]; ++test) {
+        fout << InputN[test] << " " << InputM[test] << " " <<
+            InputS[test] << " " << InputQ[test] << "\n";
+        for (int i = 0; i < InputM[test]; ++i)
+            InputE[test].insert(make_pair(InputU[test][i], InputV[test][i])),
+            InputE[test].insert(make_pair(InputV[test][i], InputU[test][i])),
+            fout << InputU[test][i] << " " << InputV[test][i] << "\n";
+        for (int i = 0; i < InputS[test]; ++i)
+            fout << InputT[test][i] << (i + 1 == InputS[test] ? "\n" : " ");
+        for (int i = 0; i < InputQ[test]; ++i)
+            fout << InputR[test][i] << (i + 1 == InputQ[test] ? "\n" : " ");
+    }
+    fout.close();
+}
+int base = 257, mod = 65537;
+inline int getHash(string str, int ret = 0) {
+    for (auto& c : str) ret = (ret * base + int(c)) % mod;
+    return ret;
+}
+inline int getHash(vector<string> vec, int ret = 0) {
+    for (auto& str : vec) ret = (ret * base + getHash(str)) % mod;
+    return ret;
+}
+inline void assertThrow(bool condition) {
+    if (!condition) throw exception();
+}
+inline void validateOutput() {
+    try {
+        OutputH.clear();
+        for (ifstream fin("out.txt"); fin >> word; OutputH.push_back(word));
+        assertThrow(getHash(OutputH) == outputHash[batch]);
+        // cout << getHash(OutputH) << endl;
+        // system("pause");
+    }
+    catch (...) {
+        endBatch("WrongAnswer");
+    }
+}
+int main(int argc, char** argv) {
+    TID = argv[1], UID = argv[2], LNG = argv[3];
+    if (LNG == "cpp") timeLimit = cpp, strcpy(cmd, "b.exe");
+    else if (LNG == "java") timeLimit = java, strcpy(cmd, "java Solution");
+    if (LNG != "py") comment = "// ";
+    else comment = "## ", timeLimit = py, strcpy(cmd, "pypy Solution.py");
+    if (!ifstream(TID + "_" + UID + "." + LNG)) updateSubmission();
+    getline(ifstream("Solution." + LNG), content, '\0');
+    for (char& c : content) c = tolower(c);
+    for (ifstream fin(TID + "_Forbidden.txt"); fin >> word;)
+        if (content.find(word) != string::npos)
+            cout << "ForbiddenKeyword: " << word << "\n", printScoreAndExit();
+    if ((LNG == "cpp" && system("c++ Solution.cpp -o b.exe")) ||
+        (LNG == "java" && system("javac Solution.java")) ||
+        (LNG == "py" && system("pypy -m py_compile Solution.py")))
+        cout << "CompilationError\n", printScoreAndExit();
+    for (string& s : vector<string>({ "RunTimeError",
+        "TimeLimitExceeded", "WrongAnswer" })) ofstream(s + ".txt").close();
+    ifstream(TID + "_" + UID + "." + LNG).ignore(3) >> best;
+    for (batch = 1; batch <= nBatch; errorCode = 0, total += weight[batch++]) {
+        rng.seed(batch), cout << "Running on Batch " << batch << endl;
+        prepareInput(), start = chrono::system_clock::now();
+        runSolution(), finish = chrono::system_clock::now();
+        if (errorCode == TLE) endBatch("TimeLimitExceeded");
+        else if (errorCode) endBatch("RunTimeError");
+        else if (validateOutput(), !errorCode && (endBatch("Accepted"),
+            best <= (score += weight[batch]))) updateSubmission();
+    }
+    cout.unsetf(ios::fixed), printScoreAndExit();
+}
