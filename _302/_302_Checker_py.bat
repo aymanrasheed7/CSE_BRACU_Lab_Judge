03dc8f54f71e4d83e6e5b35a2beeb6bb1b54e53e
unknown <aymanrasheed7@gmail.com>
2025-11-07 14:20:06 +0600

    Create _302/

---
+++ _302/_302_Checker_py.bat
@@ -0,0 +1,11 @@
+@echo off
+set TID=_302
+@REM for %%a in ("%CD%") do set TID=%%~nxa
+set SID=00000000
+set LNG=py
+c++ %TID%.cpp
+.\a.exe %TID% %SID% %LNG%
+del /f /q .\*.exe >nul 2>&1
+del /f /q .\*.class >nul 2>&1
+rd /s /q __pycache__ >nul 2>&1
+pause
